934b05a1a0cdbeb82768f29a60d4b8f0c7319ec5
Mus'ab Husaini <musabhusaini@gmail.com>
2012-12-29 00:07:25 +0200

    moving Global class to its own package as it didn't look nice to put something in the default package.

---
+++ sare-webapp/app/global/Global.java
@@ -0,0 +1,24 @@
+package global;
+import java.util.concurrent.TimeUnit;
+
+import actors.*;
+import akka.actor.*;
+import play.*;
+import play.libs.*;
+import scala.concurrent.duration.Duration;
+
+public class Global extends GlobalSettings {
+
+	@Override
+	public void onStart(Application app) {
+		super.onStart(app);
+		
+		// run session cleaner.
+		ActorRef sessionCleaner = Akka.system().actorOf(new Props(SessionCleaner.class));
+		Akka.system().scheduler().schedule(
+			Duration.create(0, TimeUnit.MILLISECONDS),
+			Duration.create(10, TimeUnit.MINUTES),
+			sessionCleaner,
+			"", Akka.system().dispatcher());
+	}
+}
