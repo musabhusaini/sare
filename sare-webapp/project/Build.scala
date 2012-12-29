029ee9d0c50799e2776a4606d601801f747fed5a
Mus'ab Husaini <musabhusaini@gmail.com>
2012-12-29 20:12:34 +0200

    making dependencies more robust and removing redundant entries.

--- sare-webapp/project/Build.scala
+++ sare-webapp/project/Build.scala
@@ -10,7 +10,12 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
 		javaCore, javaJdbc, javaEbean,
 		// Add your project dependencies here,
-		"edu.sabanciuniv.sentilab" % "sare-entitymanager" % "2.0.0-SNAPSHOT"
+		("edu.sabanciuniv.sentilab" % "sare-entitymanager" % "2.0.0-SNAPSHOT")
+			.exclude("org.apache.commons", "commons-lang3")
+			.exclude("com.google.guava", "guava")
+			.exclude("org.reflections", "reflections")
+			.exclude("joda-time", "joda-time")
+			.exclude("junit", "junit")
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
