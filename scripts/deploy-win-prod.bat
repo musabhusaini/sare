cd8ae35879025a056becf93d0f005fd2b4c87e93
Mus'ab Husaini <musabhusaini@gmail.com>
2012-12-30 14:13:22 +0200

    updating deploy scripts.

--- scripts/deploy-win-prod.bat
+++ scripts/deploy-win-prod.bat
@@ -2,5 +2,5 @@ call git pull
 call mvn clean compile install
 rd /S/Q c:\play2\repository\cache\edu.sabanciuniv.sentilab
 cd sare-webapp
-call play clean update compile run
+call play clean update compile start -Dconfig.resource=prod.conf
 cd..
