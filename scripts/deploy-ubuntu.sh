e027a3faf5e9d71a4e561928c397fc671a313c1c
Mus'ab Husaini <musabhusaini@gmail.com>
2013-03-27 11:05:07 +0200

    updating scripts to generate source and javadoc before maven install in local environments

--- scripts/deploy-ubuntu.sh
+++ scripts/deploy-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git pull
-mvn clean compile install
+mvn clean compile source:jar javadoc:jar install
 rm -rf $PLAY_HOME/repository/cache/edu.sabanciuniv.sentilab
 cd sare-webapp
 play clean compile run
