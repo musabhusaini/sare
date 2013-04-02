b9448f76f09498b47ce1c6b987b2f11077d56b08
Mus'ab Husaini <musabhusaini@gmail.com>
2013-04-02 16:19:02 +0300

    updating the deploy script a bit and making some cosmetic changes to the main page.

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -5,5 +5,5 @@ mvn clean compile install
 rm -rf $PLAY_HOME/repository/cache/edu.sabanciuniv.sentilab
 cd sare-webapp
 play clean compile stage
-./target/start -Dconfig.resource=prod.conf
+./target/start -Dconfig.resource=prod.conf &
 cd ..
