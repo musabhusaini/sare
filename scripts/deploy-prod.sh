c5bf3259a86363ea10780b04f1022514d163226c
Mus'ab Husaini <musabhusaini@gmail.com>
2013-04-15 18:35:47 +0300

    making minor fix in the script.

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -9,7 +9,7 @@ cat ../secret.key >> sare-webapp/conf/prod.conf
 
 SQL_PWD=$(<../sql.pwd)
 echo -n "db.default.password=${SQL_PWD}" >> sare-webapp/conf/prod.conf
-xmlstarlet ed -N x="http://java.sun.com/xml/ns/persistence" -u "/x:persistence/x:persistence-unit/x:properties/x:property[@name='javax.persistence.jdbc.password']/@value" -v "${SQL_PWD}" sare-entitymanager/src/main/resources/META-INFpersistence.xml
+xmlstarlet ed -N x="http://java.sun.com/xml/ns/persistence" -u "/x:persistence/x:persistence-unit/x:properties/x:property[@name='javax.persistence.jdbc.password']/@value" -v "${SQL_PWD}" sare-entitymanager/src/main/resources/META-INF/persistence.xml
 
 mvn clean compile install
 rm -rf $PLAY_HOME/repository/cache/edu.sabanciuniv.sentilab
