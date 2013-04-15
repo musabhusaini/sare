40bf7b6aef9f81a01a739442f90e4d36774905f2
Mus'ab Husaini <musabhusaini@gmail.com>
2013-04-15 18:21:10 +0300

    changing database user name and trying to create a way to have safer passwords in prod.

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+git clean -df
+git checkout .
 git pull
+
+echo -n "application.secret=" >> sare-webapp/conf/prod.conf
+cat ../secret.key >> sare-webapp/conf/prod.conf
+
+SQL_PWD=$(<../sql.pwd)
+echo -n "db.default.password=${SQL_PWD}" >> sare-webapp/conf/prod.conf
+xmlstarlet ed -N x="http://java.sun.com/xml/ns/persistence" -u "/x:persistence/x:persistence-unit/x:properties/x:property[@name='javax.persistence.jdbc.password']/@value" -v "${SQL_PWD}" sare-entitymanager/src/main/resources/META-INFpersistence.xml
+
 mvn clean compile install
 rm -rf $PLAY_HOME/repository/cache/edu.sabanciuniv.sentilab
 cd sare-webapp
--- scripts/setup-prod.sql
+++ scripts/setup-prod.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS sare_test_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL PRIVILEGES ON sare_test_db.* TO 'java_user'@'localhost' IDENTIFIED BY 'java_user_pwd';
+GRANT ALL PRIVILEGES ON sare_test_db.* TO 'sare_user'@'localhost' IDENTIFIED BY '[insert password here]';
 
 CREATE DATABASE IF NOT EXISTS sare_main_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL PRIVILEGES ON sare_main_db.* TO 'java_user'@'localhost';
+GRANT ALL PRIVILEGES ON sare_main_db.* TO 'sare_user'@'localhost';
 
 CREATE DATABASE IF NOT EXISTS sare_webapp_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL PRIVILEGES ON sare_webapp_db.* TO 'java_user'@'localhost';
+GRANT ALL PRIVILEGES ON sare_webapp_db.* TO 'sare_user'@'localhost';
