e1a3c18bf2c239fee33009d1678558de247c32ec
Mus'ab Husaini <musabhusaini@gmail.com>
2013-04-18 23:19:19 +0300

    updating scripts to add the option to skip deploying the backend.

--- scripts/deploy-standby-prod.sh
+++ scripts/deploy-standby-prod.sh
@@ -10,11 +10,22 @@ echo "application.secret=${SECRET_KEY}" >> sare-webapp/conf/prod.conf
 SQL_PWD=$(<../sql.pwd)
 echo "db.default.password=${SQL_PWD}" >> sare-webapp/conf/prod.conf
 
-xmlstarlet ed -L -N x="http://java.sun.com/xml/ns/persistence" -u "/x:persistence/x:persistence-unit/x:properties/x:property[@name='javax.persistence.jdbc.password']/@value" -v "${SQL_PWD}" sare-entitymanager/src/main/resources/META-INF/persistence.xml
+skipBackend=false
+for arg in "$@"
+do
+	if [ "$arg" == "-sb" ] ; then
+		$skipBackend = true
+	fi
+done
+
+if ! $skipBackend ; then
+	xmlstarlet ed -L -N x="http://java.sun.com/xml/ns/persistence" -u "/x:persistence/x:persistence-unit/x:properties/x:property[@name='javax.persistence.jdbc.password']/@value" -v "${SQL_PWD}" sare-entitymanager/src/main/resources/META-INF/persistence.xml
+
+	mvn clean compile install
+fi
 
 xmlstarlet ed -L -u "/configuration/appender[@name='DB']/connectionSource/dataSource/password" -v "${SQL_PWD}" sare-webapp/conf/prod-logger.xml
 
-mvn clean compile install
 cd sare-webapp
 play clean compile stage
 ./target/start -Dhttp.port=9001 -Dconfig.resource=prod.conf -Dlogger.resource=prod-logger.xml &
